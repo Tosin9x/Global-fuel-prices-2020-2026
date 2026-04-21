92a5db3f9359bf1663955a09e58f58f4a41332ac
Tosin9x <mayowagiwa9x@gmail.com>
2026-04-21 18:09:30 +0100

    first commit

---
+++ insights.sql
@@ -0,0 +1,98 @@
+-- COUNTRY AND REGIONAL COMPARISON
+-- Average petrol and diesel prices for each country.
+
+SELECT 
+    global_fuel_tb.country AS country,
+    ROUND(AVG(global_fuel_tb.petrol_usD_litre), 3) AS average_petrol_usd,
+    ROUND(AVG(global_fuel_tb.diesel_usd_litre), 3) AS average_diesel_usd
+FROM
+    global_fuel_tb
+GROUP BY country
+ORDER BY country; 
+
+-- which REGION HAS the highest AVERAGE PETROL prices.
+
+SELECT 
+	global_fuel_tb.region AS region,
+	AVG(global_fuel_tb.petrol_usD_litre) AS petrol_usd
+FROM global_fuel_tb
+GROUP BY region
+ORDER BY petrol_usd DESC;
+
+-- INCOME RELATIONSHIP WITH PRICE OF PETROL
+
+SELECT 
+	global_fuel_tb.income_level AS income_level,
+    AVG(global_fuel_tb.petrol_usD_litre) AS avg_petrol_price
+FROM 
+	global_fuel_tb
+GROUP BY income_level
+ORDER BY income_level;
+
+
+/*
+TOP and bottom perfomers in terms of price of fuel
+
+ country with the most expensive petrol prices and their levels of income
+*/
+
+SELECT 
+    global_fuel_tb.country AS country,
+    global_fuel_tb.subsidy_level AS subsidy_level,
+    global_fuel_tb.income_level AS income_level,
+    AVG(global_fuel_tb.petrol_usD_litre) AS petrol_price
+FROM
+    global_fuel_tb
+GROUP BY country, subsidy_level, income_level
+ORDER BY petrol_price DESC
+LIMIT 5;
+
+
+-- country with the cheapest petrol prices and their levels of income
+
+SELECT 
+    global_fuel_tb.country AS country,
+    global_fuel_tb.subsidy_level AS subsidy_level,
+    global_fuel_tb.income_level AS income_level,
+    ROUND(AVG(global_fuel_tb.petrol_usD_litre), 3) AS petrol_price_usd
+FROM
+    global_fuel_tb
+GROUP BY country , subsidy_level , income_level
+ORDER BY petrol_price_usd ASC
+LIMIT 10;
+
+-- Countries with high income level and low petrol prices
+
+SELECT 
+	global_fuel_tb.country AS Country,
+    global_fuel_tb.income_level AS income_level,
+    ROUND(AVG(global_fuel_tb.petrol_usD_litre), 3) AS petrol_price_usd
+FROM global_fuel_tb
+GROUP BY Country, income_level
+HAVING income_level = 'High'
+ORDER BY petrol_price_usd ASC
+LIMIT 10;
+
+-- low income countries with expensive petrol
+
+SELECT 
+    global_fuel_tb.country AS Country,
+    global_fuel_tb.income_level AS income_level,
+    ROUND(AVG(global_fuel_tb.petrol_usD_litre), 3) AS petrol_price_usd
+FROM
+    global_fuel_tb
+GROUP BY Country , income_level
+HAVING income_level = 'low'
+ORDER BY petrol_price_usd ASC
+LIMIT 10;
+
+
+-- Global price of petrol over the years
+
+SELECT 
+   YEAR(global_fuel_tb.date) AS year,
+   ROUND(AVG(global_fuel_tb.petrol_usD_litre), 3) AS avg_petrol_price
+FROM global_fuel_tb
+GROUP BY year
+ORDER BY year ASC;
+
